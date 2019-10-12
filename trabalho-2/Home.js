aca12b7134db5e63a9936b86840ad4044a2ba160
mudouasenha <mudouasenha@gmail.com>
2019-10-12 15:05:11 -0300

    pagination Home.js

--- trabalho-2/Home.js
+++ trabalho-2/Home.js
@@ -8,7 +8,9 @@ import {
   StyleSheet,
   Button,
   FlatList,
+  ScrollView,
 } from 'react-native';
+import MainServices from './services/MainServices'
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -17,70 +19,100 @@ export default class HomeScreen extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { isLoading: true };
+    this.state = { 
+      isLoading: true,
+      page: 1,
+      orgaos: []
+    };
   }
 
   componentDidMount() {
-    const { navigation } = this.props;
-    this.focusListener = navigation.addListener('didFocus', () => {
-      return fetch(
-        'http://www.transparencia.gov.br/api-de-dados/orgaos-siafi?pagina=1'
-      )
-        .then(response => response.json())
-        .then(responseJson => {
-          console.log(responseJson);
-          this.setState(
-            {
-              isLoading: false,
-              orgaos: responseJson,
-            },
-            function() {}
-          );
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    });
+    // const { navigation } = this.props;
+
+    // this.focusListener = navigation.addListener('didFocus', () => {
+      this.buscaOrgaos();
+    // });
   }
 
   componentWillUnmount() {
     this.focusListener.remove();
   }
 
-buscaPorOrgao(){
+  buscaPorOrgao() {
     var descricaoUrl = encodeURI(this.state.descricao);
     var paginaUrl = encodeURI(1);
-    var uri = "http://www.transparencia.gov.br/api-de-dados/orgaos-siafi"+
-      "?descricao="+descricaoUrl+
-      "&pagina="+paginaUrl;
+    var uri =
+      'http://www.transparencia.gov.br/api-de-dados/orgaos-siafi' +
+      '?descricao=' +
+      descricaoUrl +
+      '&pagina=' +
+      paginaUrl;
     fetch(uri)
-        .then((response) => response.json())
-        .then((responseJson) => {
+      .then(response => response.json())
+      .then(responseJson => {});
+  }
 
-  });
+  buscaOrgaos = () => {
+    const { orgaos, page } = this.state
+    this.setState({ isLoading: true})
+    return fetch(
+        `http://www.transparencia.gov.br/api-de-dados/orgaos-siafi?pagina=${page}`
+      )
+        .then(res => res.json())
+        .then(resJson => {
+          console.log(resJson);
+          this.setState({
+              isLoading: false,
+              orgaos: resJson,
+            }, () => {});
+        })
+        .catch(error => {
+          console.error(error);
+        });
+
+  }
+
+
+
+  handleNextPage = () => {
+    this.setState({
+      page: this.state.page + 1
+    }, () => {
+      this.buscaOrgaos()
+    })
+  }
+
+  handlePreviousPage = () => {
+    if( this.state.page >= 1) {
+    this.setState({
+      page: this.state.page - 1
+    }, () => {
+      this.buscaOrgaos()
+    })
+    } else {
+      console.log('erro')
+    }
   }
 
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View>
-
+      <ScrollView>
         <Text> </Text>
         <Text>Insira a descrição do órgão que dejesa consultar</Text>
         <TextInput
           onChangeText={text => this.setState({ descricao: text })}
           //value={''}
         />
-        <Text> </Text>
+        {this.state.isLoading ? <Text> Carregando... </Text> : <Text>  </Text>}
         <Button
           title="Buscar"
           onPress={() => {
             this.setState({
-              isLoading: true
-            })
-            this.buscaPorOrgao
-          }
-          }
+              isLoading: true,
+            });
+            this.buscaPorOrgao;
+          }}
         />
         <FlatList
           data={this.state.orgaos}
@@ -92,11 +124,22 @@ buscaPorOrgao(){
                   {item.codigo}, {item.descricao}
                 </Text>
               </View>
-              }
             </TouchableOpacity>
           )}
         />
-      </View>
+        <Button 
+          title="Próxima Página"
+          onPress={() => {
+            this.handleNextPage()
+          }}
+          />
+          {this.state.page === 1 && this.state.isLoading === false ? <Text></Text> : <Button 
+          title="Página Anterior"
+          onPress={() => {
+            this.handlePreviousPage()
+          }}
+          />}
+      </ScrollView>
     );
   }
 }
---
+++ trabalho-2/services/MainServices.js
@@ -0,0 +1,5 @@
+export default class MainServices {
+  load() {
+    setTimeout(3000)
+  }
+}
